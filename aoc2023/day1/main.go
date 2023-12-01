088d7ebf31dc979760eea6c00ce21d6d672ca06e
Boris Kuznetsov <l0wpressu7e@gmail.com>
2023-12-02 01:51:54 +0300

    feat: aoc2023 day1 part1 and part2 solved

--- aoc2023/day1/main.go
+++ aoc2023/day1/main.go
@@ -1,5 +1,144 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/10wpressure/advent-of-code/util"
+)
+
+const (
+	fileName  = "input.txt"
+	chunkSize = 100
+)
+
+var Digits = map[string]int{
+	"zero":  0,
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+var LiteralDigits = map[string]int{
+	"0": 0,
+	"1": 1,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
+}
+
+func FindFirstAndLastDigit(s string) int {
+	var first = make(map[int]int)
+	var last = make(map[int]int)
+	fd, ld := 0, 0
+	for k, v := range Digits {
+		ind := strings.Index(s, k)
+		indL := strings.LastIndex(s, k)
+		if ind == -1 {
+			continue
+		}
+		first[ind] = v
+		last[indL] = v
+	}
+	for k, v := range LiteralDigits {
+		ind := strings.Index(s, k)
+		indL := strings.LastIndex(s, k)
+		if ind == -1 {
+			continue
+		}
+		first[ind] = v
+		last[indL] = v
+	}
+
+	minFirstKey := math.MaxInt64
+	for k := range first {
+		if k < minFirstKey {
+			minFirstKey = k
+		}
+	}
+
+	maxLastKey := math.MinInt64
+	for k := range last {
+		if k > maxLastKey {
+			maxLastKey = k
+		}
+	}
+
+	fd = first[minFirstKey]
+	ld = last[maxLastKey]
+	//fmt.Printf("fd: %d | ld: %d\n", fd, ld)
+	return fd*10 + ld
+}
+
+func part1(f *os.File) string {
+	scanner := bufio.NewScanner(f)
+	var res = 0
+
+	var iter = 0
+	for scanner.Scan() {
+		iter++
+		line := []rune(scanner.Text())
+		fd := 0
+		ld := 0
+		for _, v := range line {
+			n, err := strconv.Atoi(string(v))
+			if err != nil {
+				continue
+			}
+			//_ = fmt.Sprintf("%d: %c\n", i, v)
+			if fd == 0 {
+				fd = n * 10
+				ld = n
+				continue
+			} else {
+				ld = n
+			}
+		}
+		res += fd + ld
+		//fmt.Printf("%d: %d | %d\n", iter, fd+ld, res)
+	}
+	return strconv.Itoa(res)
+}
+
+func part2(f *os.File) string {
+	scanner := bufio.NewScanner(f)
+	var res = 0
+
+	var iter = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		iter++
+		cur := FindFirstAndLastDigit(line)
+		res += cur
+		//fmt.Printf("%d: %s = %d (%d)\n", iter, line, cur, res)
+	}
+	return strconv.Itoa(res)
+}
+
 func main() {
+	f := util.OpenFile("input.txt")
+	defer f.Close()
+	result := part1(f)
+	fmt.Println("Part 1:", result)
+
+	f = util.OpenFile(fileName)
+	defer f.Close()
+	result = part2(f)
+	fmt.Println("Part 2:", result)
 
 }
--- aoc2023/day1/main_test.go
+++ aoc2023/day1/main_test.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"strconv"
+	"testing"
 
+	"github.com/10wpressure/advent-of-code/util"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	testFileName = "test.txt"
+)
+
+func Test(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "API Suite")
 }
+
+var _ = Describe("Day 1", func() {
+	BeforeEach(func() {
+		_ = os.Remove(testFileName)
+	})
+
+	It("Part 1 Test 1", func() {
+		err := os.WriteFile(testFileName, []byte("1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"), 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		f := util.OpenFile(testFileName)
+		defer f.Close()
+		defer os.Remove(testFileName)
+		res := part1(f)
+		Expect(res).To(Equal("142"))
+	})
+
+	It("Part 2 Test 1", func() {
+		err := os.WriteFile(testFileName, []byte("two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen\nnineeight6nine1three1eight"), 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		f := util.OpenFile(testFileName)
+		defer f.Close()
+		defer os.Remove(testFileName)
+		res := part2(f)
+		s := 281 + 98
+		Expect(res).To(Equal(strconv.Itoa(s)))
+	})
+
+})
